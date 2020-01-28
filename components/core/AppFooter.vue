95a1c1b2db813fc331843d95ee69b7bc613437d9
Inou Ridder <inouridder@gmail.com>
2020-01-28 12:19:04 +0100

    ft: introduce core components (nav/footer)

---
+++ components/core/AppFooter.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <div class="content has-text-centered">
+        <div class="soc">
+          <a href="#"><i class="fas fa-heart fa-2x" aria-hidden="true"></i></a>
+        </div>
+        <p>
+          <strong>Cookbook</strong> by <a href="http://inouridder.io">Inou Ridder</a>.
+          The source code is licensed <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. <br>
+        </p>
+      </div>
+    </div>
+  </footer>
+</template>
+
+
+<script>
+  export default {
+
+  }
+</script>
---
+++ components/core/AppNav.vue
@@ -0,0 +1,42 @@
+<template>
+  <nav class="navbar">
+    <div class="container">
+      <div class="navbar-brand">
+        <n-link class="navbar-item" to="/">
+          <img src="~/assets/graphiti-logo.png" alt="Logo">
+        </n-link>
+        <span class="navbar-burger burger" data-target="navbarMenu">
+          <span></span>
+          <span></span>
+          <span></span>
+        </span>
+      </div>
+      <div id="navbarMenu" class="navbar-menu">
+        <div class="navbar-end">
+          <n-link to='/' class="navbar-item is-active">
+            Home
+          </n-link>
+          <n-link to='/recipes/new' class="navbar-item">
+            Add a recipe
+          </n-link>
+          <div class="navbar-item has-dropdown is-hoverable">
+            <div class="navbar-dropdown">
+              <n-link to='/recipes/new' class="navbar-item">
+                Add a recipe
+              </n-link>
+              <n-link to='/' class="navbar-item is-active">
+                Home
+              </n-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
